2b38f8764de5f2bccdf7baf2239d56d8729bf64a
yirano <yirano@protonmail.ch>
2020-08-19 13:58:11 -0400

    Create endpoints for articles: user can get articles by its id, the user's id, or the category id

--- models/articles-model.js
+++ models/articles-model.js
@@ -1,3 +1,28 @@
 const db = require('../database/config')
 
-module.exports = {}
+function getArticles() {
+	return db('articles')
+}
+
+function getArticleById(id) {
+	return db('articles').where('articles.id', id)
+}
+
+function getByUser(id) {
+	return db('articles').where('articles.userID', id)
+}
+
+function getByCategory(id) {
+	return db('category_article')
+		.where('category_article.categoryID', id)
+		.join('categories', 'categories.id', 'category_article.categoryID')
+		.join('articles', 'articles.id', 'category_article.articleID')
+		.select('articles.id', 'articles.title', 'articles.desc', 'articles.url')
+}
+
+module.exports = {
+	getArticles,
+	getArticleById,
+	getByUser,
+	getByCategory
+}
--- routers/articles-router.js
+++ routers/articles-router.js
@@ -1,8 +1,33 @@
 const router = require('express').Router()
+const Articles = require('../models/articles-model')
 
 router.get('/', async (req, res, next) => {
 	try {
-		res.status(200).json({ message: 'Articles router works' })
+		res.status(200).json(await Articles.getArticles())
+	} catch (error) {
+		next(error)
+	}
+})
+
+router.get('/:id', async (req, res, next) => {
+	try {
+		res.status(200).json(await Articles.getArticleById(req.params.id))
+	} catch (error) {
+		next(error)
+	}
+})
+
+router.get('/:id/user', async (req, res, next) => {
+	try {
+		res.status(200).json(await Articles.getByUser(req.params.id))
+	} catch (error) {
+		next(error)
+	}
+})
+
+router.get('/:id/category', async (req, res, next) => {
+	try {
+		res.status(200).json(await Articles.getByCategory(req.params.id))
 	} catch (error) {
 		next(error)
 	}
