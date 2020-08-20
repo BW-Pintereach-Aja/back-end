32583ac178bad48040e268f21744db824f6f78ab
yirano <yirano@protonmail.ch>
2020-08-19 23:37:31 -0400

    Add cleaner in seeds

--- database/seeds/00_cleanup.js
+++ database/seeds/00_cleanup.js
@@ -1,4 +1,11 @@
+const cleaner = require('knex-cleaner')
+
 exports.seed = async function(knex) {
+	await cleaner.clean(knex, {
+		mode: 'truncate',
+		restartIdentity: true,
+		ignoreTables: [ 'knex_migrations', 'knex_migrations_lock' ]
+	})
 	await knex('category_article').truncate()
 	await knex('categories').truncate()
 	await knex('articles').truncate()
