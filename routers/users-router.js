282804a143dbefdf93038e9951ac6de20709c8dc
tajahouse <ahlliahtaja@yahoo.com>
2020-08-19 20:30:36 -0700

    Completed user-router

--- routers/users-router.js
+++ routers/users-router.js
@@ -14,9 +14,9 @@ router.get('/', async (req, res, next) => {
 	}
 })
 
-router.get('/users', restrict, async(req, res, next)=>{
-	try{
-		res.json( await Users.find());
+router.get('/users',  async (req, res, next) =>{
+	try {
+		res.json(await Users.find())
 	} catch (err){
 		next(err)
 	}
@@ -43,10 +43,54 @@ router.post('/register', validation, async( req, res, next) =>{
 		next(err)
 	}
 });
-router.post('/login', validation, async(req, res, next=>{
+
+router.post('/login', validation, async (req, res, next) =>{
 try{
-	cont { firstname username, password }
+	const {  firstname, lastname, username, password } = req.body
+	const user = await Users.findBy({ username }).first()
+
+	if(!user){
+		return res.status(400).json({
+			message: "Invalid Credentials"
+		})
+	}
+	const passwordValid = await bcrypt.compare(password, user.password)
+	
+	if(!passwordValid){
+		 return res.status(400).json({
+			message: "Invalid Credentials",
+		})
+	}
+	const payload = {
+		userId: user.id,
+		firstname: user.firstname,
+		lastname: user.lastname,
+		username: user.username
+	}
+	res.cookie("token", jwt.sign(payload, process.env.JWT_SECRET || "secretiveness"))
+	const token = jwt.sign(payload, process.env.JWT_SECRET || "secretiveness" )
+	res.json({
+		message: `Welcome ${user.username}!`,
+		token: token
+	})
+}catch (err){
+	next(err)
 }
 } )
+
+router.get("/logout", async (req, res, next) => {
+	try {
+		req.session.destroy((err) => {
+			if (err) {
+				next(err)
+			} else {
+				res.status(204).end()
+			}
+		})
+	} catch (err) {
+		next(err)
+	}
+})
+
 
 module.exports = router;
