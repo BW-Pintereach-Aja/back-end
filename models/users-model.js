20d9562e5a7fc5714135382433aa40a93f88afcf
yirano <yirano@protonmail.ch>
2020-08-20 11:50:30 -0400

    Test register endpoint on deployment

--- models/users-model.js
+++ models/users-model.js
@@ -8,16 +8,16 @@ module.exports = {
 }
 
 async function add(user) {
-	const id  = await db('users').insert(user).returning('id')
+	const id = await db('users').insert(user).returning('id')
 	return findById(id)
 }
 
 function find() {
-	return db("users").select("id", "username", "firstname", "lastname")
+	return db('users').select('id', 'username', 'firstname', 'lastname')
 }
 
 function findBy(filter) {
-	return db('users').select('id', 'username', 'password', 'firstname', 'lastname').where(filter)
+	return db('users').select('id', 'username', 'password', 'firstname', 'lastname').where('users.username', filter)
 }
 
 function findById(id) {
--- routers/users-router.js
+++ routers/users-router.js
@@ -16,38 +16,35 @@ router.get('/', async (req, res, next) => {
 })
 
 //get users
-router.get('/users',  async (req, res, next) =>{
+router.get('/users', async (req, res, next) => {
 	try {
 		res.json(await Users.find())
-	} catch (err){
+	} catch (err) {
 		next(err)
 	}
 })
 
 //get users by id
-router.get("/users/:id", validateUser, (req, res, next)=>{
-	try{
-			res.json(req.user)
-		const { user } = req.session;
-    if (user.id !== Number(req.params.id))
-      return res
-        .status(401)
-		.json({ message: "You are unable to access this resource." });
+router.get('/users/:id', validateUser, (req, res, next) => {
+	try {
+		res.json(req.user)
+		const { user } = req.session
+		if (user.id !== Number(req.params.id))
+			return res.status(401).json({ message: 'You are unable to access this resource.' })
 		next()
-
-	} catch(err){
+	} catch (err) {
 		next(err)
 	}
 })
 
 //register user
-router.post('/register', validation, async( req, res, next) =>{
-	try{
-		const { firstname, lastname, username, password } = req.body;
-		const user = await Users.findBy({ username }).first()
-		if (user){
+router.post('/register', async (req, res, next) => {
+	try {
+		const { firstname, lastname, username, password } = req.body
+		const user = await Users.findBy(username).first()
+		if (user) {
 			res.status(409).json({
-				message: "User already taken"
+				message: 'User already taken'
 			})
 		}
 		const newUser = await Users.add({
@@ -55,52 +52,50 @@ router.post('/register', validation, async( req, res, next) =>{
 			lastname,
 			username,
 			password: await bcrypt.hash(password, 10)
-		});
+		})
 		res.status(201).json(newUser)
-	} catch(err){
+	} catch (err) {
 		next(err)
 	}
 })
 
-
 //login
-router.post('/login', validation, async (req, res, next) =>{
-try{
-	const {  firstname, lastname, username, password } = req.body
-	const user = await Users.findBy({ username }).first()
+router.post('/login', validation, async (req, res, next) => {
+	try {
+		const { firstname, lastname, username, password } = req.body
+		const user = await Users.findBy({ username }).first()
 
-	if(!user){
-		return res.status(400).json({
-			message: "Invalid Credentials"
-		})
-	}
-	const passwordValid = await bcrypt.compare(password, user.password)
-	
-	if(!passwordValid){
-		 return res.status(400).json({
-			message: "Invalid Credentials",
-		})
-	}
-	const payload = {
-		userId: user.id,
-		firstname: user.firstname,
-		lastname: user.lastname,
-		username: user.username
-	}
-	res.cookie("token", jwt.sign(payload, process.env.JWT_SECRET || "secretiveness"))
-	const token = jwt.sign(payload, process.env.JWT_SECRET || "secretiveness" )
-	res.json({
-		message: `Welcome ${user.username}!`,
-		token: token
-	})
-}catch (err){
-	next(err)
-}
-} )
+		if (!user) {
+			return res.status(400).json({
+				message: 'Invalid Credentials'
+			})
+		}
+		const passwordValid = await bcrypt.compare(password, user.password)
 
+		if (!passwordValid) {
+			return res.status(400).json({
+				message: 'Invalid Credentials'
+			})
+		}
+		const payload = {
+			userId: user.id,
+			firstname: user.firstname,
+			lastname: user.lastname,
+			username: user.username
+		}
+		res.cookie('token', jwt.sign(payload, process.env.JWT_SECRET || 'secretiveness'))
+		const token = jwt.sign(payload, process.env.JWT_SECRET || 'secretiveness')
+		res.json({
+			message: `Welcome ${user.username}!`,
+			token: token
+		})
+	} catch (err) {
+		next(err)
+	}
+})
 
 //ogout
-router.get("/logout", async (req, res, next) => {
+router.get('/logout', async (req, res, next) => {
 	try {
 		req.session.destroy((err) => {
 			if (err) {
@@ -114,5 +109,4 @@ router.get("/logout", async (req, res, next) => {
 	}
 })
 
-
-module.exports = router;
+module.exports = router
